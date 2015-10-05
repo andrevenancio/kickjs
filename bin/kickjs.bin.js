c967b5f73f8d8e61436af3dcd0818d9c64c441d3
André Venâncio <andre.venancio@holler.com.au>
2015-10-06 10:47:06 +1100

    updating cp command

--- bin/kickjs.bin.js
+++ bin/kickjs.bin.js
@@ -74,8 +74,8 @@ function copyTemplate( onComplete ) {
 
 	if ( fs.existsSync( templatePath ) ) {
 		clearFolder( function() {
-			bash( 'cp -r ' + templatePath + '/ ' + process.cwd(), function() {
-				bash( 'cp -r ' + corePath + '/ ' + process.cwd(), function() {
+			bash( 'cp -R ' + templatePath + '/ ' + process.cwd(), function() {
+				bash( 'cp -R ' + corePath + '/ ' + process.cwd(), function() {
 					onComplete();
 				} );
 			} );
